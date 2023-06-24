77672477ca73c913f10c135f566a39aa161bde14
chidiebere uroko <urokochidieberep@outlook.com>
2023-06-23 20:35:44 -0700

    isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit
+ * @c: The character to be checked
+ * Return: 1 for digit charater or 0 for any other thing
+ */
+
+int _isdigit(int c)
+
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+{
+return (0);
+}
+}
